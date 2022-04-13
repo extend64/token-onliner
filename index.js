739f20ff539ba1387b517a285975350403bbb8fb
Extend <57357197+extend64@users.noreply.github.com>
2022-04-13 04:07:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,25 +10,8 @@ function joinVoice(token) {
   client.login(token)
   
   client.on('ready', () => {
-    
     console.log(`${client.user.tag} is online!`);
-    
-    const channel = client.channels.cache.get(channelID);
-    
-    if(!channel) return;
-    
-    channel.join().then(c => {
-      
-      c.voice.setSelfDeaf(false); // kulaklık
-      c.voice.setSelfMute(true); // mikrofon
-      
-      console.log(`Ses kanalına girildi`)
-      
-      }).catch(e => {
-        console.error(e);
-      })
   })
-}
 
 const tokens = File.readFileSync('./tokens.txt', 'utf-8').split('\n');
 
